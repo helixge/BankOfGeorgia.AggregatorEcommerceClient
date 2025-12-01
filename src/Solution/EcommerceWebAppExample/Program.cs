35ed44bbff7df5963c56c549cc40e2e95679b01b
Toka <toka.gelashvili@visma.com>
2025-12-01 17:19:47 +0400

    Update webapp example

--- src/Solution/EcommerceWebAppExample/Program.cs
+++ src/Solution/EcommerceWebAppExample/Program.cs
@@ -5,16 +5,15 @@ using Microsoft.AspNetCore.Mvc;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
-//builder.AddBankOfGeorgiaAggregatorEcommerce<BogAggregatorCallbackHandler>();
-builder.Services
-    .AddBankOfGeorgiaAggregatorEcommerce<BogAggregatorCallbackHandler>(builder.Configuration);
+builder.AddBankOfGeorgiaAggregatorEcommerce<BogAggregatorCallbackHandler>();
+// Or if you want to use an extension for IServiceCollection, call:
+// builder.Services.AddBankOfGeorgiaAggregatorEcommerce<BogAggregatorCallbackHandler>(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseBankOfGeorgiaCallback("/api/bog/callback");
+app.UseBankOfGeorgiaCallback("/bog/callback");
 
-app.MapGet(
-    "/return/bog/success",
+app.MapGet("/bog/return/success",
     (
         ILogger<Program> logger,
         [FromQuery] string? orderId = null
@@ -23,8 +22,7 @@ app.MapGet(
         logger.LogInformation("Received success return for Order ID: {OrderId}", orderId);
     });
 
-app.MapGet(
-    "/return/bog/fail",
+app.MapGet("/bog/return/fail",
     (
         ILogger<Program> logger,
         [FromQuery] string? orderId = null
@@ -33,5 +31,57 @@ app.MapGet(
         logger.LogWarning("Received fail return for Order ID: {OrderId}", orderId);
     });
 
-await app.RunAsync();
+app.MapGet("/",
+    (
+        ILogger<Program> logger,
+        [FromQuery] string? orderId = null
+    ) =>
+    {
+        return Results.Content("""
+            <form action="pay" method="post" enctype="multipart/form-data">
+                Amount: <input type="number" name="amount" value="2.3" step="0.01">
+                <br/>
+                <button type="submit">Pay</button>
+            </form>
+            """, "text/html");
+    });
 
+app.MapPost("/pay",
+    async (
+        ILogger<Program> logger,
+        IBankOfGeorgiaAggregatorEcommerceClient bog,
+        [FromForm] decimal amount
+    ) =>
+    {
+        string orderId = Guid.NewGuid().ToString();
+        SubmitOrderRequest request = new()
+        {
+            ExternalOrderId = orderId,
+            CallbackUrl = "https://mywebsite.ge/bog/callback",
+            RedirectUrls = new RedirectUrls()
+            {
+                Success = $"http://localhost:4444/bog/return/success?id={orderId}",
+                Fail = $"http://localhost:4444/bog/return/fail?id={orderId}"
+            },
+            PaymentMethod = [PaymentMethod.Card],
+            PurchaseUnits = new PurchaseUnits()
+            {
+                TotalAmount = amount,
+                Basket =
+                [
+                    new BasketItem()
+                    {
+                        ProductId = "Example product",
+                        Quantity = 1,
+                        UnitPrice = amount,
+                    },
+                ]
+            },
+
+            // Other options propeties
+        };
+        SubmitOrderResponse response = await bog.SubmitOrder(request);
+        return Results.Redirect(response.RedirectLink, permanent: false);
+    }).DisableAntiforgery();
+
+await app.RunAsync();
